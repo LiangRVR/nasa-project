c437f24d4885ee1ccb20462a4c7b78ba75e0f5b1
LiangRVR <liangvillarrubia95@gmail.com>
2022-08-12 10:53:08 -0400

    refactor: export all mongo_URl from .env instead of just user and password

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -2,10 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const mongoService = {};
 
-const dbUsername = process.env.DB_USERNAME;
-const dbPassword = process.env.DB_PASSWORD;
-
-const MONGO_URL = `mongodb+srv://${dbUsername}:${dbPassword}@nasa-db.zklge.mongodb.net/nasa?retryWrites=true&w=majority`;
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connected");
